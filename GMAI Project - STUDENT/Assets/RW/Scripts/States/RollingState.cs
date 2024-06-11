850f5dd796dd5a611381faeaf1afdb665dcd174b
Ian Wong <ianw35091@gmail.com>
2024-06-11 15:55:22 +0800

    Three Anims Pt.3

--- GMAI Project - STUDENT/Assets/RW/Scripts/States/RollingState.cs
+++ GMAI Project - STUDENT/Assets/RW/Scripts/States/RollingState.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class RollingState : MonoBehaviour
+namespace RayWenderlich.Unity.StatePatternInUnity
 {
-    // Start is called before the first frame update
-    void Start()
+    public class RollingState : State
     {
-        
-    }
+        private bool rolling;
+        private float rollForce = 5f;
+        private int rollParam = Animator.StringToHash("Roll");
+        public RollingState(Character character, StateMachine stateMachine) : base(character, stateMachine)
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            rolling = true;
+            character.TriggerAnimation(rollParam);
+            character.ApplyImpulse(Vector3.forward * rollForce);
+        }
+
+        public override void LogicUpdate()
+        {
+            base.LogicUpdate();
+
+        }
     }
 }
