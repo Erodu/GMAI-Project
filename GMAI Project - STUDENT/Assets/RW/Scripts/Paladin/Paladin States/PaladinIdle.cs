dfd60becdf3b7c4acf69f3f557a3fc8cd40c1f46
Ian Wong <ianw35091@gmail.com>
2024-06-14 17:31:46 +0800

    Paladin Pt.1

---
+++ GMAI Project - STUDENT/Assets/RW/Scripts/Paladin/Paladin States/PaladinIdle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaladinIdle : PaladinStates
+{
+    public PaladinIdle(Paladin m_Paladin)
+    {
+        paladin = m_Paladin;
+    }
+
+    public override void Enter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- GMAI Project - STUDENT/Assets/RW/Scripts/Paladin/Paladin.cs
+++ GMAI Project - STUDENT/Assets/RW/Scripts/Paladin/Paladin.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.AI;
 
 public class Paladin : MonoBehaviour
 {
+    #region States
+
+    public PaladinStates m_Idle { get; set; } = null;
+
+    public PaladinStates m_Current { get; set; } = null;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Idle = new PaladinIdle(this);
+        m_Current = m_Idle;
+        m_Current.Enter();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_Current.Execute();
     }
+
+    #endregion
 }
